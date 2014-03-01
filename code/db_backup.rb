b04c6b667b9be4e96f1d22e089e9c18b53189420
Kannan Thambiah <pygospa@gmail.com>
2014-03-01 21:53:19 +0100

    Add *.sql to gitignore + bugfix in db_backup.rb

--- code/db_backup.rb
+++ code/db_backup.rb
@@ -3,13 +3,14 @@
 database = ARGV.shift
 username = ARGV.shift
 password = ARGV.shift
+
 end_of_iter = ARGV.shift
 
 
 if end_of_iter.nil?
-  backup_file = config[:database] + '_' + Time.now.strftime('%Y%m%d')
+  backup_file = database + Time.now.strftime("%Y%m%d")
 else
-  backup_file = config[:database] + '_' + end_of_iter
+  backup_file = database + end_of_ite
 end
 
 `mysqldump -u#{username} - p#{password} #{database} > #{backup_file}.sql`
